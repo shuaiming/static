3fdd18d0222840675daa95e8864c633f44557411
周帅明 <smzhou@corp.netease.com>
2021-08-21 22:03:03 +0800

    serve static files

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Copyright 2021 shuaimingzhou(at)gmail(dot)com.
+// All rights reserved. License that can be found in the LICENSE file.
+
+/* serve static files */
+package static
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shuaiming/static
+
+go 1.16
